324dbf55fc117c4235bb76fe9d0a5294b85aeac0
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2024-03-15 23:44:48 +0100

    opravený footer

--- code/app.py
+++ code/app.py
@@ -20,15 +20,15 @@ async def main_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.route("/prijmy")
-async def main_page(request: Request):
+async def prijmy(request: Request):
     return templates.TemplateResponse("prijmy.html", {"request": request})
 
 @app.route("/vydaje")
-async def main_page(request: Request):
+async def vydaje(request: Request):
     return templates.TemplateResponse("vydaje.html", {"request": request})
 
 @app.route("/kurzy")
-async def main_page(request: Request):
+async def kurzy(request: Request):
     try:
         rate_usd = database.get_usd()
     except Exception as e:
@@ -40,5 +40,5 @@ async def main_page(request: Request):
     return templates.TemplateResponse("kurzy.html", {"request": request, "usd_rate": rate_usd, "eur_rate": rate_eur})
 
 @app.route("/souhrn")
-async def main_page(request: Request):
+async def souhrn(request: Request):
     return templates.TemplateResponse("souhrn.html", {"request": request})
